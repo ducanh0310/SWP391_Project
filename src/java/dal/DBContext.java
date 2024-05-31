1caa81f72d282c26aa14d29db7e6c632ca8b94a4
Nguyễn Phương Nam <140996779+ngphnam@users.noreply.github.com>
2024-05-31 23:06:11 +0700

    merge config

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -20,7 +20,7 @@ public class DBContext {
         Connection connection2 = null;
         try {
             String user = "sa";
-            String pass = "123121";
+            String pass = "namnp";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP391_Project;encrypt=true;trustServerCertificate=true";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection2 = DriverManager.getConnection(url, user, pass);
