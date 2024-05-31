b06e6f48d3ddac91945b9033fc6191ea9d10db31
ducanh0310 <nguyenducanhflm@gmail.com>
2024-05-31 22:33:22 +0700

    update addPatient.jsp

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -14,15 +14,15 @@ import java.util.logging.Logger;
  *
  * @author ngphn
  */
-public abstract class DBContext{
+public class DBContext{
     
     protected Connection connection;
     
-    protected DBContext() {
+    public DBContext() {
         try {
             String user = "sa";
-            String pass = "30020102";
-            String url = "jdbc:sqlserver://WINDYVU:1433;databaseName=SWP391_Project;encrypt=true;trustServerCertificate=true;";
+            String pass = "123456";
+            String url = "jdbc:sqlserver://DUCANH\\SQLEXPRESS:1433;databaseName=SWP391_Project;encrypt=true;trustServerCertificate=true;";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
@@ -31,5 +31,8 @@ public abstract class DBContext{
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+        System.out.println("hello" + db);
+    }
 }
