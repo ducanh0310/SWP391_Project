d9c969a044857ac3738d997f1bdcbe810a894959
Nguyễn Phương Nam <140996779+ngphnam@users.noreply.github.com>
2024-06-05 01:11:11 +0700

    update

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -33,7 +33,7 @@ public class DBContext {
         Connection connection2 = null;
         try {
             String user = "sa";
-            String pass = "123456";
+            String pass = "namnp";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP391_Project;encrypt=true;trustServerCertificate=true";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection2 = DriverManager.getConnection(url, user, pass);
